e2dbd1694f45db008c6186f7b6385421771e3f9e
namespace <122353362+sdk17586@users.noreply.github.com>
2023-11-06 20:54:25 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# novnc 설치 - 이 부분은 우분투의 표준 저장소에 novnc가 없다면 필요합니다.
-# apt-get을 통해 직접 설치할 수 없다면 소스에서 직접 클론하는 것이 필요합니다.
+# novnc 설치
 git clone https://github.com/novnc/noVNC.git /opt/noVNC
 ln -s /opt/noVNC/vnc.html /opt/noVNC/index.html
 git clone https://github.com/novnc/websockify /opt/noVNC/utils/websockify
@@ -12,5 +11,16 @@ dpkg -i /root/web_vnc_view/turbovnc_3.0.3_amd64.deb
 # VNC 비밀번호 설정
 echo "rdv12345678!" | /opt/TurboVNC/bin/vncpasswd -f > /opt/TurboVNC/bin/passwd
 
-# VNC 서버 버전 확인 (실제 사용에서는 필요하지 않을 수 있음)
-/opt/TurboVNC/bin/vncserver -version
+# VNC 서버 시작을 위한 기본 설정 파일 생성 (필요한 경우)
+/opt/TurboVNC/bin/vncserver -kill :1 || true
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak || true
+echo "#!/bin/sh" > ~/.vnc/xstartup
+echo "unset SESSION_MANAGER" >> ~/.vnc/xstartup
+echo "unset DBUS_SESSION_BUS_ADDRESS" >> ~/.vnc/xstartup
+echo "[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup" >> ~/.vnc/xstartup
+echo "[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources" >> ~/.vnc/xstartup
+echo "xsetroot -solid grey" >> ~/.vnc/xstartup
+echo "vncconfig -iconic &" >> ~/.vnc/xstartup
+echo "autocutsel -fork" >> ~/.vnc/xstartup
+echo "startxfce4 &" >> ~/.vnc/xstartup
+chmod +x ~/.vnc/xstartup
