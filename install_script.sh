d7d3f24db79d6985bb9221f7553cce1b47c56fe0
namespace <122353362+sdk17586@users.noreply.github.com>
2023-11-06 20:49:00 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo apt install novnc
+sudo apt install novnc
 sudo dpkg -i /root/web_vnc_view/turbovnc_3.0.3_amd64.deb
 echo "rdv1234!" | /opt/TurboVNC/bin/vncpasswd -f > /opt/TurboVNC/bin/passwd
 
