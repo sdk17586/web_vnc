5a70164b454eb48f9ace949035dc17f459bb14b4
namespace <122353362+sdk17586@users.noreply.github.com>
2023-11-06 20:47:33 +0900

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -2,6 +2,8 @@
 
 #sudo apt install novnc
 sudo dpkg -i /root/web_vnc_view/turbovnc_3.0.3_amd64.deb
+echo "your_password" | /opt/TurboVNC/bin/vncpasswd -f > /opt/TurboVNC/bin/passwd
+
 git clone https://github.com/novnc/websockify noVNC/utils/websockify
 
 /opt/TurboVNC/bin/vncserver -version
